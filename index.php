50d067501cb5d19242a3369670c48be4c7bdcfef
Eftakhairul Islam <eftakhairul@gmail.com>
2015-06-21 17:43:01 -0400

    code intentation is added

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
 /** 
- *  
- * @author      Eftakhairul Islam <eftakhairul@gmail.com>  http://eftakhairul.com
+ * Main File  
+ *
+ * @author Eftakhairul Islam <eftakhairul@gmail.com>  http://eftakhairul.com
  */
  
 include_once('yahoosearch.php');
 
-$request = $_REQUEST['msg'];
+$request 		= $_REQUEST['msg'];
+$search 		=  new YahooSearch();
 
-$search =  new YahooSearch();
-$xml = $search->getXml($request);
-$resultCount = $search->getResultCount($xml);
-$results = $search->getResultAsArray($xml);
-$count = 1;
+$xml 			= $search->getXml($request);
+$resultCount 	= $search->getResultCount($xml);
+$results 	    = $search->getResultAsArray($xml);
+$count 			= 1;
 
-echo "{$resultCount} results are found for you<br>";
+echo "{$resultCount} results are found for you <br>";
 
-foreach($results AS $row) {
-	echo "{$count}. ".$row->description." LinK: ".$row->link."<br>";
+foreach($results AS $row) 
+{
+	echo "{$count}. " . $row->description . " LinK: " . $row->link . "<br>";
 	$count++;
 }
